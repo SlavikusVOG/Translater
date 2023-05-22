a87116b4db5edbb2fd0d1f46e55af9772bd40ea7
SlavikusVOG <slavaolish@gmail.com>
2023-05-22 23:57:47 +0300

    feat: config server app

---
+++ server/db.ts
@@ -0,0 +1,26 @@
+const mongoose = require('mongoose');
+
+async function main() {
+  await mongoose.connect('mongodb://127.0.0.1:27017/test');
+
+  // use `await mongoose.connect('mongodb://user:password@127.0.0.1:27017/test');` if your database has auth enabled
+}
+
+main().catch(err => console.log(err));
+
+const cardSchema = new mongoose.Schema({
+    word: String,
+    translate: String
+});
+
+const Card = mongoose.model('Card', cardSchema);
+
+const count = Card.countDocuments({}, (err, count) => {
+    if (count < 1) {
+        const defaultWord = new Card({ word: 'Привет мир', translate: 'Hello world'});
+        defaultWord.save().then(() => console.log('meow'));
+    }
+});
+
+
+export { };
