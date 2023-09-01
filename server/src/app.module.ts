87d99161d73db6060ee9cedfdc89e3888235c5f3
SlavikusVOG <slavaolish@gmail.com>
2023-09-01 19:11:47 +0300

    feat: add word  instance

--- server/src/app.module.ts
+++ server/src/app.module.ts
@@ -1,9 +1,14 @@
 import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { WordsModule } from './words/words.module';
 
 @Module({
-  imports: [],
+  imports: [
+    WordsModule,
+    MongooseModule.forRoot('mongodb://admin:admin@localhost:27017/translater'),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
